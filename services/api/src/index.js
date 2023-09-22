f0ff53276857c60c30f3427ef8360d4422b235af
gaollxll <hitgxless@gmail.com>
2023-09-22 16:07:54 -0400

    add docker file to api service

---
+++ services/api/src/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send({
+    success: 'hit on api service'
+  });
+});
+
+
+app.listen(port, () => {
+  console.log('app is running on ', port);
+});
